1021bcfba9aac4b7502d1df9fe9e174f7c6dd751
Madison Slansky <madisonslansky@Madisons-Air.attlocal.net>
2020-12-11 15:22:14 -0600

    endpoint heroku

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://git.heroku.com/serene-dawn-88685.git',
+  API_ENDPOINT: 'https://serene-dawn-88685.herokuapp.com',
   API_TOKEN: '5432a9d2-f030-4d76-b8df-1409026e4b54'
 }
